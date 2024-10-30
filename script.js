55d43fd86ad92049e3ecae8d7636cd104f702443
DavidCRandall <142061108+ilovepi31415@users.noreply.github.com>
2024-10-29 21:39:37 -0400

    Dynamic width and smooth color changing

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const answerA = document.getElementById('a');
 const answerB = document.getElementById('b');
 const answerC = document.getElementById('c');
+const answerContainer = document.getElementById('answer-container');
+
 function hello(button) {
     button.style.backgroundColor = '#fff';
 }
+
+// Changes the orientation of the answers based on the screen width
+setInterval(() => {
+    answerContainer.style.flexDirection = (
+        window.innerWidth < 1200 ? 'column' : 'row'
+    );
+}, 100);
