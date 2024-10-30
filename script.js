88484254985dbdcb8ac104b899b14424869ce15a
DavidCRandall <142061108+ilovepi31415@users.noreply.github.com>
2024-10-29 22:06:40 -0400

    Simple Responses on Answers

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const buttons = document.querySelectorAll('#answer-container button')
 const answerA = document.getElementById('a');
 const answerB = document.getElementById('b');
 const answerC = document.getElementById('c');
 const answerContainer = document.getElementById('answer-container');
+const feedback = document.getElementById('feedback');
 
-function hello(button) {
+function answer(button) {
     button.style.backgroundColor = '#fff';
+    buttons.forEach((btn) => {
+        btn.disabled = true;
+    })
+    setFeedback('Correct!');
 }
 
 // Changes the orientation of the answers based on the screen width
@@ -13,3 +19,7 @@ setInterval(() => {
         window.innerWidth < 1200 ? 'column' : 'row'
     );
 }, 100);
+
+function setFeedback(message) {
+    feedback.textContent = message;
+}
