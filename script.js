99398b83e443fa4f2547786532eb4b169afa0bed
DavidCRandall <142061108+ilovepi31415@users.noreply.github.com>
2024-10-29 20:58:29 -0400

    Basic Layout

---
+++ script.js
@@ -0,0 +1,6 @@
+const answerA = document.getElementById('a');
+const answerB = document.getElementById('b');
+const answerC = document.getElementById('c');
+function hello(button) {
+    button.style.backgroundColor = '#fff';
+}
